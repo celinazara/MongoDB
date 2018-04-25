096a50b003d5a8e6ec46390e842cdf356a612673
Celina <celina.zaragoza@gmail.com>
2018-04-25 13:52:59 -0500

    controller.js

---
+++ models/article.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var articleSchema = new Schema ({
+    title: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    comment: [{
+        type: Schema.Types.ObjectId,
+        ref: "comment"
+    }]
+});
+
+var article = mongoose.model("article", articleSchema);
+
+module.exports = article;
---
+++ models/comment.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var commentSchema = new Schema({
+    name: {
+        type: String
+    },
+    body:{
+        type: String,
+        required: true
+    }
+});
+
+var comment = mongoose.model("comment", commentSchema);
+
+module.exports = comment;
